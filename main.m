835748dfa6a3caf3cc8f94388c8efbac8c9893e4
Andrzej Rafalski <anraf1001@gmail.com>
2022-05-26 16:18:16 +0200

    customize measured data plot window

--- main.m
+++ main.m
@@ -6,7 +6,10 @@ Tp = 0.08;
 %% Wczytanie oraz wyświetlenie danych pomiarowych
 load('dane.mat');
 
-figure;
+fig_meas_data = figure('Position', [100 100 1200 900],...
+                       'Name', 'Dane pomiarowe',...
+                       'NumberTitle', 'off');
+
 subplot(2, 1, 1);
 plot((0:length(in) - 1) * Tp, in);
 title('Sygnał wejściowy - napięcie nagrzewnicy');
