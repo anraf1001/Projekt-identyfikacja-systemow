bdbe537444ea0769c672d0d18de6891605e9008c
Andrzej Rafalski <anraf1001@gmail.com>
2022-05-27 12:45:59 +0200

    create time vector

--- main.m
+++ main.m
@@ -5,19 +5,21 @@ Tp = 0.08;
 
 %% Wczytanie oraz wyświetlenie danych pomiarowych
 load('dane.mat');
+N = length(in);
+t = (0:length(in) - 1) * Tp;
 
 fig_meas_data = figure('Position', [100 100 1200 900],...
                        'Name', 'Dane pomiarowe',...
                        'NumberTitle', 'off');
 
 subplot(2, 1, 1);
-plot((0:length(in) - 1) * Tp, in);
+plot(t, in);
 title('Sygnał wejściowy - napięcie nagrzewnicy');
 xlabel('t [s]');
 grid on
 
 subplot(2, 1, 2);
-plot((0:length(out) - 1) * Tp, out);
+plot(t, out);
 title('Sygnał wyjściowy - napięcie termopary');
 xlabel('t [s]');
 grid on
